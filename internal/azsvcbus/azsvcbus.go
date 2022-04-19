3600d989032e46348ad1f03d035a2cf4532d1caf
Sudarsan Raghavan <camelinx@gmail.com>
2022-04-19 12:19:25 -0700

    Generate uinque uuid per sender and receiver combination instead of numeric ids

--- internal/azsvcbus/azsvcbus.go
+++ internal/azsvcbus/azsvcbus.go
@@ -1,13 +1,13 @@
 package azsvcbus
 
 import (
-    "os"
     "sync"
     "time"
     "context"
     "fmt"
 
     "github.com/golang/glog"
+    "github.com/google/uuid"
     "github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 )
 
@@ -24,7 +24,7 @@ func NewAzSvcBus( )( *AzSvcBus ) {
 func ( azSvcBus *AzSvcBus )Start( ) {
     client, err := azservicebus.NewClientFromConnectionString( azSvcBus.ConnStr, nil )
     if err != nil {
-        glog.Errorf( "Failed to setup Azure Service Bus client %v\n", err )
+        glog.Errorf( "Failed to setup Azure Service Bus client %v", err )
         return
     }
 
@@ -37,52 +37,58 @@ func ( azSvcBus *AzSvcBus )Start( ) {
 
     azSvcBus.ctx = ctx
 
+    uuidsLen := azSvcBus.TotSenders
+    if uuidsLen < azSvcBus.TotReceivers {
+        uuidsLen = azSvcBus.TotReceivers
+    }
+
+    uuids := make( [ ]string, uuidsLen )
+    for i := 0; i < azSvcBus.TotSenders; i++ {
+        uuids[ i ] = uuid.New( ).String( )
+    }
+
     if !azSvcBus.SenderOnly {
         azSvcBus.wg.Add( azSvcBus.TotReceivers )
-        for i := 1; i <= azSvcBus.TotReceivers; i++ {
-            go azSvcBus.receiveMessage( i )
+        for i := 0; i < azSvcBus.TotReceivers; i++ {
+            go azSvcBus.receiveMessage( uuids[ i ] )
         }
     }
 
     if !azSvcBus.ReceiverOnly {
         azSvcBus.wg.Add( azSvcBus.TotSenders )
-        for i := 1; i <= azSvcBus.TotSenders; i++ {
-            go azSvcBus.sendMessage( i )
+        for i := 0; i < azSvcBus.TotSenders; i++ {
+            go azSvcBus.sendMessage( uuids[ i ] )
         }
     }
 
     azSvcBus.wg.Wait( )
 }
 
-func ( azSvcBus *AzSvcBus )getId( id int )( idStr string ) {
-    return fmt.Sprintf( "%v:%v", id, os.Getpid( ) )
-}
-
-func ( azSvcBus *AzSvcBus )sendMessage( id int ) {
+func ( azSvcBus *AzSvcBus )sendMessage( id string ) {
     sender, err := azSvcBus.client.NewSender( azSvcBus.TopicName, nil )
     if err != nil {
-        glog.Errorf( "%v: Failed to create sender, error = %v\n", id, err )
+        glog.Errorf( "%v: Failed to create sender, error = %v", id, err )
         azSvcBus.wg.Done( )
         return
     }
 
     defer func( ) {
-        glog.Infof( "%v: Sender done\n", id )
+        glog.Infof( "%v: Sender done", id )
         sender.Close( azSvcBus.ctx )
         azSvcBus.wg.Done( )
     }( )
 
-    msg := fmt.Sprintf( "%v:%v:%v:%v", os.Getpid( ), time.Now( ).Format( time.UnixDate ), id, message )
+    msg := fmt.Sprintf( "%v:%v:%v", time.Now( ).Format( time.UnixDate ), id, message )
 
     azsvcbusmsg := &azservicebus.Message{
         Body                  : [ ]byte( msg ),
-        ApplicationProperties : map[ string ]interface{ }{ azSvcBus.PropName : azSvcBus.getId( id ) },
+        ApplicationProperties : map[ string ]interface{ }{ azSvcBus.PropName : id },
     }
 
     for {
         err = sender.SendMessage( azSvcBus.ctx, azsvcbusmsg, nil )
         if err != nil {
-            glog.Errorf( "%v: Failed to send message, error = %v\n", id, err )
+            glog.Errorf( "%v: Failed to send message, error = %v", id, err )
             return
         }
 
@@ -92,27 +98,25 @@ func ( azSvcBus *AzSvcBus )sendMessage( id int ) {
     return
 }
 
-func ( azSvcBus *AzSvcBus )receiveMessage( id int ) {
+func ( azSvcBus *AzSvcBus )receiveMessage( id string ) {
     receiver, err := azSvcBus.client.NewReceiverForSubscription( azSvcBus.TopicName, azSvcBus.SubName, nil )
     if err != nil {
-        glog.Errorf( "%v: Failed to create receiver, error = %v\n", id, err )
+        glog.Errorf( "%v: Failed to create receiver, error = %v", id, err )
         azSvcBus.wg.Done( )
         return
     }
 
     defer func( ) {
-        fmt.Printf( "%v: Receiver done\n", id )
+        glog.Infof( "%v: Receiver done", id )
         receiver.Close( azSvcBus.ctx )
         azSvcBus.wg.Done( )
     }( )
 
-    rcvid := azSvcBus.getId( id )
-
     for {
-        glog.Infof( "%v: Waiting to receive messages\n", id )
+        glog.Infof( "%v: Waiting to receive messages", id )
         messages, err := receiver.PeekMessages( azSvcBus.ctx, 1, nil )
         if err != nil {
-            glog.Errorf( "%v: Failed to receive messages, error = %v\n", id, err )
+            glog.Errorf( "%v: Failed to receive messages, error = %v", id, err )
             return
         }
 
@@ -120,18 +124,19 @@ func ( azSvcBus *AzSvcBus )receiveMessage( id int ) {
             propVal, exists := message.ApplicationProperties[ azSvcBus.PropName ]
             if exists {
                 sndid, ok := propVal.( string )
-                if ok && rcvid == sndid {
+                if ok && id == sndid {
+                    glog.Infof( "%v: Ignoring message from self", id )
                     continue
                 }
             }
 
             msg, err := message.Body( )
             if err != nil {
-                fmt.Printf( "%v: Failed to get received message body, error = %v\n", id, err )
+                glog.Infof( "%v: Failed to get received message body, error = %v", id, err )
                 break
             }
 
-            fmt.Printf( "%v: Received message [%v]\n", id, string( msg ) )
+            glog.Infof( "%v: Received message [%v]", id, string( msg ) )
         }
 
         time.Sleep( azSvcBus.ReceiveInterval )
